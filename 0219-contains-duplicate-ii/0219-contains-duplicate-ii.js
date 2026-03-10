70dcd276939ef80db44ba1cd890bd194de045457
Lenin838 <leninjoseph32@gmail.com>
2026-03-10 13:04:07 +0530

    Time: 793 ms (5.09%), Space: 65.5 MB (97.76%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,15 +4,10 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    let set = new Set();
-    for(let i=0;i<nums.length;i++){
-        if(set.has(nums[i])){
-            return true;
-        }
-        set.add(nums[i]);
-        if(set.size > k){
-            set.delete(nums[i-k])
-        }
+   for(let i=0;i<nums.length;i++){
+    for(let j=i+1;j<=k+i&&j<nums.length;j++){
+        if(nums[i] === nums[j]) return true;
     }
-    return false;
+   }
+   return false;
 };
