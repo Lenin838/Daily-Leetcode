bfcecbaa6945ce058f79b7eb138478b58ce019cc
Lenin Joseph <leninjoseph32@gmail.com>
2026-06-24 10:24:23 +0530

    Time: 46 ms (73.11%), Space: 58.1 MB (19.54%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let fast = head;
+    let slow = head;
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(fast === slow){
+            let ptr = head;
+            while(ptr !== slow){
+                ptr = ptr.next;
+                slow = slow.next
+            }
+            return ptr
+        }
+    }
+    return null;
+};
