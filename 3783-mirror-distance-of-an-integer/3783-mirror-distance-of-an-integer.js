03abbcaf02bc7d64911a885a59307095a7a95253
Lenin Joseph <leninjoseph32@gmail.com>
2026-06-25 20:40:46 +0530

    Time: 1 ms (70.91%), Space: 55.6 MB (52.35%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var mirrorDistance = function(n) {
+    let original = n;
+    let reverse = 0;
+
+    while (n > 0) {
+        reverse = reverse * 10 + (n % 10);
+        n = Math.floor(n / 10);
+    }
+
+    return Math.abs(original - reverse);
+};
