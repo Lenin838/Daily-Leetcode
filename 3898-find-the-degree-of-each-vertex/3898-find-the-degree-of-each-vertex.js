6183c6ddbf98c3a2954c22e95e24659480617cee
Lenin Joseph <leninjoseph32@gmail.com>
2026-06-25 17:09:43 +0530

    Time: 3 ms (55.4%), Space: 63 MB (44.6%) - LeetHub

---
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var findDegrees = function(matrix) {
+    let degrees = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        let count = 0;
+
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 1) {
+                count++;
+            }
+        }
+
+        degrees.push(count);
+    }
+
+    return degrees;
+};
