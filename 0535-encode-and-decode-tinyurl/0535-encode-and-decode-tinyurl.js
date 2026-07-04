10754ff64dd1c652176f9d9eca2487fa2649033a
Lenin Joseph <leninjoseph32@gmail.com>
2026-07-04 13:12:09 +0530

    Time: 51 ms (41.18%), Space: 55.4 MB (50%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.js
@@ -0,0 +1,29 @@
+const map = new Map();
+let id = 0;
+
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var encode = function(longUrl) {
+   const shortUrl = "http://tinyurl.com/" + id++;
+    map.set(shortUrl,longUrl);
+    return shortUrl;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+    return map.get(shortUrl);
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
